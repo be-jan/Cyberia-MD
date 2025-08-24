f3fbfddf07351a23d4a77ede5c25afb666c030e2
be-jan <berahm84@gmail.com>
2025-08-24 05:28:24 +0430

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with 
-  OWNER_NUMBER: "27649342626", // PUT OWNER NUMBER HERE
+  SESSION_ID: "malvin~dmUTWK6a#ZMz_abE2sn24fGjnaOMPID-WpmS6-o7OyQA95XHtuAs",  // add your Session Id make sure it starts with 
+  OWNER_NUMBER: "93744671474", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
